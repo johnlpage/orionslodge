ee51ba2958a68e1496ea90ec402facef208a104f
John Page <johnlpage@gmail.com>
2020-06-08 18:24:46 +0100

    formatting

--- robots/feeding/feed.py
+++ robots/feeding/feed.py
@@ -8,40 +8,42 @@ from pprint import pprint
 from pymongo import MongoClient
 
 with open("/home/pi/.mongo_uri") as fp:
-  uri = fp.readline().rstrip()
+    uri = fp.readline().rstrip()
 client = MongoClient(uri)
 calibration = -8
 
-#cs = client.vision.food.watch()
-with serial.Serial('/dev/ttyACM0', 115200, timeout=1) as ser:
+# cs = client.vision.food.watch()
+with serial.Serial("/dev/ttyACM0", 115200, timeout=1) as ser:
     while True:
-       try:
-           line = ser.readline()   # read a '\n' terminated line
-           if line != b'':
-              str = line.decode("utf-8")
-              print(str) 
-              reading = json.loads(str) 
-              reading["s"] = hex(int(reading["s"]) & 0xffffffff) 
-              if reading["n"]=="feed":
-                temp = int(reading['v']) + calibration            
-                now = datetime.datetime.now() 
-                doc = {'time':now,'sensor':reading["s"],'value':temp} 
-                rval = client.vision.food.update_one({"_id":"feeding"},{'$set':doc},upsert=True)
-                print(doc,rval)
-              if reading["n"]=="temp":
-                temp = int(reading['v']) + calibration            
-                now = datetime.datetime.now() 
-                doc = {'time':now,'sensor':reading["s"],'value':temp} 
-                rval = client.home.temp.insert_one(doc)
-                print(doc,rval)
-       except Exception as e:
-           print(e)
-           #print(line)
+        try:
+            line = ser.readline()  # read a '\n' terminated line
+            if line != b"":
+                str = line.decode("utf-8")
+                print(str)
+                reading = json.loads(str)
+                reading["s"] = hex(int(reading["s"]) & 0xFFFFFFFF)
+                if reading["n"] == "feed":
+                    temp = int(reading["v"]) + calibration
+                    now = datetime.datetime.now()
+                    doc = {"time": now, "sensor": reading["s"], "value": temp}
+                    rval = client.vision.food.update_one(
+                        {"_id": "feeding"}, {"$set": doc}, upsert=True
+                    )
+                    print(doc, rval)
+                if reading["n"] == "temp":
+                    temp = int(reading["v"]) + calibration
+                    now = datetime.datetime.now()
+                    doc = {"time": now, "sensor": reading["s"], "value": temp}
+                    rval = client.home.temp.insert_one(doc)
+                    print(doc, rval)
+        except Exception as e:
+            print(e)
+            # print(line)
 
-#       a = cs.try_next()
+        #       a = cs.try_next()
 
-       a = None
-       if a != None:
-          print(a)
-          l = a["fullDocument"]["letter"]+"\n"
-          ser.write(l.encode())
+        a = None
+        if a != None:
+            print(a)
+            l = a["fullDocument"]["letter"] + "\n"
+            ser.write(l.encode())
