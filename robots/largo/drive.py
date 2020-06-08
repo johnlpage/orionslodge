e292389a4a9e72dd08f15a6fa4b639b2ce55c850
John Page <johnlpage@gmail.com>
2020-06-08 18:23:31 +0100

    Formatted and changed to pigpio

--- robots/largo/drive.py
+++ robots/largo/drive.py
@@ -1,127 +1,143 @@
 #!/usr/bin/python
 import time
 import os
-import RPi.GPIO as GPIO
 import time
 import pymongo
-from gpiozero import Servo
 from time import sleep
 import smbus
 import time
 from pymongo import ReturnDocument
+import pigpio
+
+pi = pigpio.pi()
 
 bus = smbus.SMBus(1)
 address = 0x23
-myGPIO=18
+myGPIO = 12
+pi.set_mode(myGPIO, pigpio.OUTPUT)
 
-GPIO.setmode(GPIO.BCM)
+# GPIO.setmode(GPIO.BCM)
 energytomove = 250
 yellow = 17
 white = 22
-grey= 23
+grey = 23
 green = 24
 
-pinset=[yellow,grey,white,green]
+pinset = [yellow, grey, white, green]
 
 for pin in pinset:
-    GPIO.setup(pin,GPIO.OUT)
+    # GPIO.setup(pin,GPIO.OUT)
+    pi.set_mode(pin, pigpio.OUTPUT)
 
-left1=green
-left2=white
-right1=yellow
-right2=grey
+left1 = green
+left2 = white
+right1 = yellow
+right2 = grey
 
 with open("/home/pi/.mongo_uri") as fp:
-  uri = fp.readline().rstrip()
+    uri = fp.readline().rstrip()
 client = pymongo.MongoClient(uri)
 
 coll = client.motion.votes
 framecol = client.vision.frames
 
-def adjust_track(track1,track2,dir):
+
+def adjust_track(track1, track2, dir):
     if dir == 1:
-        GPIO.output(track1,True)
-        GPIO.output(track2,False)
+        pi.write(track1, True)
+        pi.write(track2, False)
     if dir == -1:
-        GPIO.output(track1,False)
-        GPIO.output(track2,True)
+        pi.write(track1, False)
+        pi.write(track2, True)
     if dir == 0:
-        GPIO.output(track1,False)
-        GPIO.output(track2,False)
+        pi.write(track1, False)
+        pi.write(track2, False)
 
-def adjust_car(left,right):
-    adjust_track(left1,left2,left)
-    adjust_track(right1,right2,right)
 
-#Connect to MongoDB and Get next set of instructions - plus
-#Reset votes
+def adjust_car(left, right):
+    adjust_track(left1, left2, left)
+    adjust_track(right1, right2, right)
+
+
+# Connect to MongoDB and Get next set of instructions - plus
+# Reset votes
 def getdir(doc):
-  bestvote=0;
-  bestdir=None
-  for dir in ('fwd','back','left','right','open','close'):
-    if doc.get(dir,0) > bestvote:
-        bestvote = doc.get(dir,0)
-        bestdir = dir
-  return bestdir
+    bestvote = 0
+    bestdir = None
+    for dir in ("fwd", "back", "left", "right", "open", "close"):
+        if doc.get(dir, 0) > bestvote:
+            bestvote = doc.get(dir, 0)
+            bestdir = dir
+    print(bestdir)
+    return bestdir
+
+
 count = 0
-left=0
-right=0
-runfor=0
-votelen=1
-luxspeed=10000
+left = 0
+right = 0
+runfor = 0
+votelen = 1
+luxspeed = 10000
 
 
 while True:
     lux = bus.read_word_data(address, 0x10)
     print(lux)
-    framecol.update_one({'_id':'umberto'},{'$set':{'lux':lux},'$inc':{'energy':int(lux/luxspeed+10)}},upsert=True)
-    energy = framecol.find_one_and_update({'_id':'umberto'},{'$set':{'lux':lux},'$min':{'energy':10000}},projection={'energy':1},return_document=ReturnDocument.AFTER)
-    doc = coll.find_one_and_replace({"_id":"umberto"}, {},upsert=True);
+    framecol.update_one(
+        {"_id": "umberto"},
+        {"$set": {"lux": lux}, "$inc": {"energy": int(lux / luxspeed + 10)}},
+        upsert=True,
+    )
+    energy = framecol.find_one_and_update(
+        {"_id": "umberto"},
+        {"$set": {"lux": lux}, "$min": {"energy": 10000}},
+        projection={"energy": 1},
+        return_document=ReturnDocument.AFTER,
+    )
+    doc = coll.find_one_and_replace({"_id": "umberto"}, {}, upsert=True)
     print(energy)
-    dir = getdir(doc);    
+    dir = getdir(doc)
     if dir == None:
-        count=count+1
-    if dir == 'open':
-       myServo = Servo(myGPIO)
-       myServo.min()
-       time.sleep(0.5)
-       myServo.close()
-    if dir == 'close':
-       myServo = Servo(myGPIO)
-       myServo.max()
-       time.sleep(0.5)
-       myServo.close()
-    if dir == 'fwd':
-     left=1
-     right=1
-     runfor=1
-    if dir == 'right':
-     right=-1
-     left=1
-     adjust_car(left,right)
-     time.sleep(0.15)
-     runfor = 0
-    if dir == 'left' :
-     right=1
-     left=-1
-     adjust_car(left,right)
-     time.sleep(0.15)
-     runfor = 0
-    if dir == 'back' : 
-     right=-1
-     left = -1
-     runfor=1
-    if runfor == 0 :
-        left=0
-        right=0
-        count=0
+        count = count + 1
+    if dir == "open":
+        pi.set_servo_pulsewidth(myGPIO, 1000)
+    if dir == "close":
+        pi.set_servo_pulsewidth(myGPIO, 2000)
+    if dir == "fwd":
+        left = 1
+        right = 1
+        runfor = 1
+    if dir == "right":
+        right = -1
+        left = 1
+        adjust_car(left, right)
+        time.sleep(0.15)
+        runfor = 0
+    if dir == "left":
+        right = 1
+        left = -1
+        adjust_car(left, right)
+        time.sleep(0.15)
+        runfor = 0
+    if dir == "back":
+        right = -1
+        left = -1
+        runfor = 1
+    if runfor == 0:
+        left = 0
+        right = 0
+        count = 0
     else:
-       runfor=runfor-votelen
-    if not ( left != 0 and left == right and  energy < energytomove ):
-      adjust_car(left,right)
-      if left != 0 and left == right :
-        print("Spending Energy to move")
-        framecol.update_one({'_id':'umberto'},{'$inc':{'energy':int(-energytomove)}},upsert=True);
-        
+        runfor = runfor - votelen
+    if not (left != 0 and left == right and energy < energytomove):
+        adjust_car(left, right)
+        if left != 0 and left == right:
+            print("Spending Energy to move")
+            framecol.update_one(
+                {"_id": "umberto"},
+                {"$inc": {"energy": int(-energytomove)}},
+                upsert=True,
+            )
+
     time.sleep(votelen)
 GPIO.cleanup()
--- robots/largo/vision.py
+++ robots/largo/vision.py
@@ -10,40 +10,44 @@ import bson
 import time
 import zlib
 
-vision_width=128
-vision_height=96
+vision_width = 128
+vision_height = 96
 camera = PiCamera()
 camera.resolution = (vision_width, vision_height)
 camera.color_effects = None
 camera.sensor_mode = 0
 camera.iso = 800
-#camera.rotation = 180
+# camera.rotation = 180
 camera.brightness = 55
 
 rawCapture = PiRGBArray(camera)
 time.sleep(0.5)
-name="petbot"
+name = "petbot"
 
 with open("/home/pi/.mongo_uri") as fp:
-  uri = fp.readline().rstrip()
+    uri = fp.readline().rstrip()
 client = pymongo.MongoClient(uri)
 
 db = client.vision
-coll=db.frames
+coll = db.frames
 try:
-    coll.insert_one({'_id':'umberto'})
+    coll.insert_one({"_id": "umberto"})
 except:
-    pass;
+    pass
 
 
-
-#cycle through the stream of images from the camera
+# cycle through the stream of images from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    image = frame.array 
-    image = image[:,:,2]
-    bytes = image.tobytes();
-    zbytes = zlib.compress(bytes)    
-    r = { 'x':vision_width,'y':vision_height,'data':bson.binary.Binary(zbytes),'when':datetime.datetime.utcnow()}
-    coll.update_one({'_id':'umberto'},{ '$set' : r })
+    image = frame.array
+    image = image[:, :, 2]
+    bytes = image.tobytes()
+    zbytes = zlib.compress(bytes)
+    r = {
+        "x": vision_width,
+        "y": vision_height,
+        "data": bson.binary.Binary(zbytes),
+        "when": datetime.datetime.utcnow(),
+    }
+    coll.update_one({"_id": "umberto"}, {"$set": r})
     rawCapture.truncate(0)
     time.sleep(0.1)
